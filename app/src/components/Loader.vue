af2d712c1df0d6856fb0eed7e1bfabc139b88ac7
Brasilio Thomazo <brasilio.thomazo@gmail.com>
2022-01-26 05:17:54 -0300

    Adicionado o componente loader

---
+++ app/src/components/Loader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="className">
+    Carregando
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+</template>
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Loader',
+  props: {
+    visible: Boolean,
+  },
+  setup(props) {
+    const className = computed(() =>
+      props.visible ? 'loader show' : 'loader',
+    );
+    return { className };
+  },
+});
+</script>
